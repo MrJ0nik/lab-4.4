f216cba240743f4479a6a7b4212e7cf457ac05fd
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-09-22 01:19:51 +0300

    first commit

---
+++ lab 4.4/lab 4.4.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <iomanip> 
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double y, x, Xpoc, Xkinc, Xkrok;
+    cout << "Xpoc = "; cin >> Xpoc;
+    cout << "Xkinc = "; cin >> Xkinc;
+    cout << "Xkrok = "; cin >> Xkrok;
+
+    x = Xpoc;  // Ініціалізація x значенням Xpoc
+
+    cout << fixed;
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(5) << "x" << " |"
+        << setw(7) << "y" << " |" << endl;
+    cout << "---------------------------" << endl;
+
+    while (x <= Xkinc)
+    {
+        if (x <= -4)
+        {
+            y = -2;
+        }
+        else
+        {
+            if (-4 < x && x <= 0)
+                y = x / 4;
+            else if (0 < x && x <= 2)
+                y = x * x;
+            else
+                y = 5 - (1.0 / 2.0) * x; 
+
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << y
+            << " |" << endl;
+
+        x += Xkrok;
+    }
+
+    cout << "---------------------------" << endl;
+    return 0;
+}
